a6396c8c258378e7c33d9de91966948c9cce0e0e
Paul <owoeyepaul456@gmail.com>
2025-04-01 18:14:50 -0700

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,13 @@ import {
 } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import MainLayouts from "./layouts/MainLayouts";
-import PricePage from "./PricePage";
-
+import PricePage from "./Pages/PricePage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayouts />}>
       <Route index element={<HomePage />} />
-      <Route path="*" element={<PricePage/>}/>
+      <Route path="*" element={<PricePage />} />
     </Route>
   )
 );
